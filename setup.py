11ed83fa36077a6816e5b0641d2cb5c322e8dcfd
Thomas Burrell <98192078+thomasbva@users.noreply.github.com>
2023-07-31 14:44:57 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='M09_Booklover_package',
+    version='1.0',
+    author='Thomas Burrell',
+    author_email='tmb9ccd@virginia.edu',
+    description='Allow user to add books to a list, rate their books.'
+)
