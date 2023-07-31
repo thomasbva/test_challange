94ac8321dbec1d05d7491583241c904ac0ec08b8
Thomas Burrell <98192078+thomasbva@users.noreply.github.com>
2023-07-31 14:45:22 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='M09_Booklover_package',
+    name='Upper_package',
     version='1.0',
     author='Thomas Burrell',
     author_email='tmb9ccd@virginia.edu',
-    description='Allow user to add books to a list, rate their books.'
+    description='Allow user to uppercase any string.'
 )
